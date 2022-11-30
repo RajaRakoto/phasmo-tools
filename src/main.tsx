5185a9a5edb34a87aef568bd018c82c807e7e7a2
raja <raja.rakoto7@gmail.com>
2022-11-30 16:01:47 +0300

    init main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/* components */
+import Phasmo from './components/phasmo';
+
+/* styles */
+import './main.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<Phasmo />
+	</React.StrictMode>,
+);
