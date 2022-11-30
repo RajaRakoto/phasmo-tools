f4f844117ad7ce0da96b96d14b09f7bc952b455f
raja <raja.rakoto7@gmail.com>
2022-11-30 18:52:13 +0300

    safezone

--- src/components/phasmo.tsx
+++ src/components/phasmo.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
+/* assets */
+import phasmo_logo from '../assets/phasmo-logo.png';
+
 /* styles */
 import './phasmo.min.css';
 
 export default function phasmo() {
-	return <div>phasmo</div>;
+	return (
+		<React.Fragment>
+			<div className="phasmo">
+				<img src={phasmo_logo} alt="phasmo tools logo" />
+			</div>
+		</React.Fragment>
+	);
 }
