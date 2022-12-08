f95a65f61726f9b5d476f5727eab7fa19f22dd9f
raja <raja.rakoto7@gmail.com>
2022-12-08 22:57:32 +0300

    add EffectCreative swiper | enterBtn (sub component)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,54 +2,79 @@ import React, { useState } from 'react';
 
 /* libs */
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCards, Autoplay } from 'swiper';
+import { Autoplay, EffectCreative } from 'swiper';
 
 /* assets */
 import phasmo_logo from '../assets/phasmo-logo.png';
 import * as pack from '../../package.json';
 
+/* data */
+import { banner } from '../data/_banner';
+
 /* styles */
 import './app.min.css';
 import 'swiper/css';
-import 'swiper/css/effect-cards';
+import 'swiper/css/effect-creative';
 
-function Banner() {
+/* interfaces */
+interface FeaturesProps {
+	title: string;
+	lists: string[];
+	icon: any;
+}
+
+// ================================================
+
+function Banner({ enterBtn }: any) {
 	return (
 		<header className="container d-flex justify-content-center flex-column align-items-center">
-			<img
-				className="logo anim--bounce"
-				src={phasmo_logo}
-				alt="phasmophobia tools logo"
-			/>
+			<img className="logo" src={phasmo_logo} alt="phasmophobia tools logo" />
 			<span className="version_author">
 				version {pack.version} - by {pack.author.split(' ')[0]}
 			</span>
 			<h1>Bienvenue</h1>
-			<p className="description">
-				Cet outil vous aidera à trouver des indices, des stratégies contre des
-				entités, connaître les emplacements des objets et bien d'autres
-				astuces... Découvrez tout cela en cliquant sur le bouton ci-dessous !
-			</p>
+			<p className="description">{banner.bannerInformation}</p>
+			{enterBtn}
 			<Swiper
 				className="swiper-container"
-				effect={'cards'}
 				grabCursor={true}
-				centeredSlides={true}
+				effect={'creative'}
+				creativeEffect={{
+					prev: {
+						shadow: true,
+						translate: ['-120%', 0, -500],
+					},
+					next: {
+						shadow: true,
+						translate: ['120%', 0, -500],
+					},
+				}}
 				autoplay={{
-					delay: 2500,
+					delay: 4500,
 					disableOnInteraction: false,
 				}}
-				modules={[EffectCards, Autoplay]}
+				modules={[EffectCreative, Autoplay]}
 			>
-				<SwiperSlide>lorem</SwiperSlide>
-				<SwiperSlide>lorem</SwiperSlide>
-				<SwiperSlide>lorem</SwiperSlide>
-				<SwiperSlide>lorem</SwiperSlide>
+				{banner.features.map((feature: FeaturesProps, index: number) => (
+					<SwiperSlide key={'feat-card-' + index}>
+						<div className="swiper-item">
+							<span className="swiper-icon">{feature.icon}</span>
+							<h2>{feature.title}</h2>
+							<hr />
+							<ul>
+								{feature.lists.map((list, index) => (
+									<li key={'feat-list-' + index}>{list}</li>
+								))}
+							</ul>
+						</div>
+					</SwiperSlide>
+				))}
 			</Swiper>
 		</header>
 	);
 }
 
+// TODO: add router
 function Core() {
 	return (
 		<p>
@@ -62,21 +87,25 @@ function Core() {
 }
 
 export default function App() {
-	const version: number = 10;
 	const [bannerVisibility, setBannerVisibility] = useState(true);
 
 	const handleBanner = () => {
 		setBannerVisibility(!bannerVisibility);
 	};
 
+	const EnterBtn = () => {
+		return (
+			<button className="enter-btn phasmo--btn" onClick={handleBanner}>
+				ENTRER
+			</button>
+		);
+	};
+
 	return (
 		<React.Fragment>
 			{bannerVisibility ? (
 				<div id="banner">
-					<Banner />
-					<button className="phasmo--btn" onClick={handleBanner}>
-						ENTRER
-					</button>
+					<Banner enterBtn={<EnterBtn />} />
 				</div>
 			) : (
 				<Core />
