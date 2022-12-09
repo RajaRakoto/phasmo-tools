6244d4f76a87c6db20416dbd348e46cd14f8b942
raja <raja.rakoto7@gmail.com>
2022-12-09 14:09:12 +0300

    add components

---
+++ src/app/_entities.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// ================================================
+
+export default function Entities() {
+	return (
+		<>
+			<h1>Entities</h1>
+		</>
+	);
+}
---
+++ src/app/_objects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// ================================================
+
+export default function Objects() {
+	return (
+		<>
+			<h1>Objects</h1>
+		</>
+	);
+}
