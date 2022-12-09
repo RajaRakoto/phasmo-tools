7bb70728d9b82e2624d0d7325550051c5591e3b8
raja <raja.rakoto7@gmail.com>
2022-12-09 15:39:19 +0300

    add utils component

---
+++ src/app/_utils.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// ================================================
+
+export default function Utils() {
+	return (
+		<section>
+			<h1>Utils</h1>
+		</section>
+	);
+}
