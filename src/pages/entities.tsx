db101934b9b385a56b8195f2646e63c6661e0d62
raja <raja.rakoto7@gmail.com>
2022-12-12 22:00:30 +0300

    apply HeaderSection

--- src/pages/entities.tsx
+++ src/pages/entities.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /* common */
-import { IconSectionRender } from '../common/icons';
+import { HeaderSection } from '../common/header';
 
 /* libs */
 import { FaGhost } from 'react-icons/fa';
@@ -16,13 +16,9 @@ export default function Entities() {
 	return (
 		<React.Fragment>
 			<section>
-				<div className="header">
-					<IconSectionRender icon={<FaGhost />} />
-					<h1>Entités</h1>
-				</div>
+				<HeaderSection icon={<FaGhost />} title="Entités" />
 				<div className="content">
-					{/* <Todo title="General todo" todoID="todo-general" addInput deleteBtn />
-					<Todo title="Temp todo" todoID="todo-temp" /> */}
+					<Todo title="General todo" todoID="todo-general" addInput deleteBtn />
 					<Tracker />
 				</div>
 			</section>
