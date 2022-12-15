0cf7ec3228e5efb0b7ee5a620e9adcad3802fa64
raja <raja.rakoto7@gmail.com>
2022-12-15 12:20:37 +0300

    valid floatnav sys

--- src/pages/entities.tsx
+++ src/pages/entities.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 /* common */
 import { HeaderSection } from '../common/header';
 import ScrollUp from '../common/scrollup/_scrollup';
+import FloatNav from '../common/floatnav';
 
 /* libs */
 import { FaGhost } from 'react-icons/fa';
@@ -12,7 +13,6 @@ import Todo from '../components/todo';
 import Tracker from '../components/tracker';
 import Note from '../components/note';
 import Timer from '../components/timer';
-import FloatNav from '../components/floatnav';
 
 // ================================================
 
@@ -20,7 +20,20 @@ export default function Entities() {
 	return (
 		<React.Fragment>
 			<section>
-				<FloatNav />
+				<FloatNav>
+					<a href="#note-link">
+						<button className="float-nav-btn">note</button>
+					</a>
+					<a href="#todo-link">
+						<button className="float-nav-btn">todo</button>
+					</a>
+					<a href="#tracker-link">
+						<button className="float-nav-btn">tracker</button>
+					</a>
+					<a href="#timer-link">
+						<button className="float-nav-btn">timer</button>
+					</a>
+				</FloatNav>
 				<HeaderSection icon={<FaGhost />} title="Entités" />
 				<div className="content">
 					<Todo title="General todo" todoID="todo-general" addInput deleteBtn />
--- src/pages/objects.tsx
+++ src/pages/objects.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 /* common */
 import { HeaderSection } from '../common/header';
 import ScrollUp from '../common/scrollup/_scrollup';
+import FloatNav from '../common/floatnav';
 
 /* libs */
 import { FaObjectGroup } from 'react-icons/fa';
@@ -13,8 +14,32 @@ export default function Objects() {
 	return (
 		<React.Fragment>
 			<section>
+				<FloatNav>
+					<a href="#lorem-link">
+						<button className="float-nav-btn">lorem</button>
+					</a>
+				</FloatNav>
 				<HeaderSection icon={<FaObjectGroup />} title="Objets" />
-				<div className="content"></div>
+				<div className="content">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+						facere neque fugiat suscipit accusantium nesciunt, voluptates
+						molestias enim, reiciendis totam temporibus unde dolor ut! Ullam
+						sequi recusandae incidunt minus officia.
+					</p>
+					<p id="lorem-link">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+						facere neque fugiat suscipit accusantium nesciunt, voluptates
+						molestias enim, reiciendis totam temporibus unde dolor ut! Ullam
+						sequi recusandae incidunt minus officia.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+						facere neque fugiat suscipit accusantium nesciunt, voluptates
+						molestias enim, reiciendis totam temporibus unde dolor ut! Ullam
+						sequi recusandae incidunt minus officia.
+					</p>
+				</div>
 			</section>
 			<ScrollUp />
 		</React.Fragment>
