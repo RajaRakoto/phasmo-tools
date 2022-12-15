b41dcfbe84dc920f139a39cb6dd9ce3d62413339
raja <raja.rakoto7@gmail.com>
2022-12-15 12:20:23 +0300

    reorganize

---
+++ src/common/floatnav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// ================================================
+
+export default function FloatNav({ children }: any) {
+	return (
+		<React.Fragment>
+			<div className="float-nav">{children}</div>
+		</React.Fragment>
+	);
+}
