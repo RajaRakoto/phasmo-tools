6c3d07ad04a50281848ac55385d772a18627c100
raja <raja.rakoto7@gmail.com>
2022-12-16 17:41:08 +0300

    add & apply utils tracker -> basicFilterCore() | advancedFilterCore() | exactlyFilterCore()

--- src/data/tracker.ts
+++ src/data/tracker.ts
@@ -14,23 +14,92 @@ type entitiesArrayType = {
 // init state - tracker
 const initialState: entitiesArrayType = __entitiesData_;
 
+// utils - tracker
+function basicFilterCore(
+	entities: entitiesArrayType,
+	includeEdvidences: string[],
+) {
+	if (includeEdvidences.length === 0) {
+		return entities;
+	} else if (includeEdvidences.length === 1) {
+		return entities.filter((entity: any) => {
+			return entity.edvidencesID.some((id: any) =>
+				includeEdvidences.includes(id),
+			);
+		});
+	} else {
+		return entities.filter((entity: any) => {
+			return includeEdvidences.every((id: any) =>
+				entity.edvidencesID.includes(id),
+			);
+		});
+	}
+}
+
+function advancedFilterCore(
+	entities: entitiesArrayType,
+	includeEdvidences: string[],
+	excludeEdvidences: string[],
+) {
+	return entities.filter(entity => {
+		if (includeEdvidences.length > 0) {
+			for (const edvidence of includeEdvidences) {
+				if (entity.edvidencesID.includes(edvidence)) {
+					break;
+				}
+				return false;
+			}
+		}
+		if (excludeEdvidences.length > 0) {
+			for (const edvidence of excludeEdvidences) {
+				if (entity.edvidencesID.includes(edvidence)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	});
+}
+
+function exactlyFilterCore(
+	entities: entitiesArrayType,
+	includeEdvidences: string[],
+	totalEvd: number = 3,
+) {
+	if (includeEdvidences.length === totalEvd) {
+		return entities.filter(entity => {
+			return includeEdvidences.every((id: any) =>
+				entity.edvidencesID.includes(id),
+			);
+		});
+	}
+}
+
 // slice - tracker
 const trackerSlice = createSlice({
 	name: 'tracker',
 	initialState,
 	reducers: {
-		filter__tracker: (state, action: PayloadAction<string[]>) => {
+		filter__tracker: (state, action: PayloadAction<any>) => {
 			console.info('REDUX -> filter__tracker');
-			if (action.payload.length === 0) {
-				return initialState;
-			} else if (action.payload.length === 1) {
-				return initialState.filter(entity => {
-					return entity.edvidencesID.some(id => action.payload.includes(id));
-				});
+			const includeEdvidences = action.payload[0];
+			const excludeEdvidences = action.payload[1];
+
+			// for mimic filter
+			if (includeEdvidences.length === 4) {
+				return exactlyFilterCore(initialState, includeEdvidences, 4);
+			}
+			// for others entities filter
+			else if (includeEdvidences.length === 3) {
+				return exactlyFilterCore(initialState, includeEdvidences);
+			} else if (excludeEdvidences.length === 0) {
+				return basicFilterCore(initialState, includeEdvidences);
 			} else {
-				return initialState.filter(entity => {
-					return action.payload.every(id => entity.edvidencesID.includes(id));
-				});
+				return advancedFilterCore(
+					initialState,
+					includeEdvidences,
+					excludeEdvidences,
+				);
 			}
 		},
 
