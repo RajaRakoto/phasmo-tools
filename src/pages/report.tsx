aed3baba9065d6a1ab35f74065ac92bda5ad5e1d
raja <raja.rakoto7@gmail.com>
2022-12-17 09:59:14 +0300

    init report pages

---
+++ src/pages/report.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+/* common */
+import ScrollUp from '../common/scrollup/_scrollup';
+
+// ================================================
+
+export default function Report() {
+	return (
+		<React.Fragment>
+			<section>
+				<div className="content">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+						facere neque fugiat suscipit accusantium nesciunt, voluptates
+						molestias enim, reiciendis totam temporibus unde dolor ut! Ullam
+						sequi recusandae incidunt minus officia.
+					</p>
+				</div>
+			</section>
+			<ScrollUp />
+		</React.Fragment>
+	);
+}
