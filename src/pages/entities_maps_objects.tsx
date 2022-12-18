75ed7baff4da34696c27bc5cf7064aa5bd4494b8
raja <raja.rakoto7@gmail.com>
2022-12-18 12:52:26 +0300

    hotfix | reorganize

--- src/pages/entities_maps_objects.tsx
+++ src/pages/entities_maps_objects.tsx
@@ -27,8 +27,66 @@ import Chrono from '../extras/chrono/chrono';
 export default function Entities_Maps_Objects() {
 	const entitiesDescription =
 		'Les entités paranormales sont les différents ennemis que vous affronterez durant vos missions. En utilisant votre matériel pour récolter des preuves, votre mission est de déterminer le type d’entité qui hante les lieux sans vous-même trépasser.';
+
 	const mapsDescription =
 		'Les maps sont les différents lieux que vous explorerez durant vos missions.';
+
+	const Entities_Section = () => {
+		return (
+			<section>
+				<HeaderSection
+					icon={<FaGhost />}
+					title="Entités"
+					description={entitiesDescription}
+				/>
+				<div className="content">
+					<Generals />
+					<div className="d-flex flex-row flex-wrap align-content-center justify-content-center">
+						<Todo title="Todo" todoID="todo-general" addInput deleteBtn />
+						<Timer />
+						<Chrono />
+					</div>
+					<Tracker />
+					<div className="d-flex flex-row flex-wrap align-content-center justify-content-center">
+						<Note />
+						<Questions />
+					</div>
+				</div>
+			</section>
+		);
+	};
+
+	const Maps_Section = () => {
+		return (
+			<section>
+				<HeaderSection
+					icon={<TfiMapAlt />}
+					title="Maps"
+					description={mapsDescription}
+				/>
+				<div className="content">
+					<Maps />
+				</div>
+			</section>
+		);
+	};
+
+	const Objects_Section = () => {
+		return (
+			<section>
+				<HeaderSection icon={<FaObjectGroup />} title="Objets" />
+				<div className="content">
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+						sed velit sapiente. Corrupti, vel doloremque nemo incidunt impedit
+						molestias enim eos eum voluptatem. Non explicabo minus voluptatum
+						ipsa blanditiis distinctio?
+					</p>
+				</div>
+			</section>
+		);
+	};
+
 	return (
 		<React.Fragment>
 			<FloatNav>
@@ -68,47 +126,9 @@ export default function Entities_Maps_Objects() {
 					<button className="float-nav-btn">N/A</button>
 				</a>
 			</FloatNav>
-			<section>
-				<HeaderSection
-					icon={<FaGhost />}
-					title="Entités"
-					description={entitiesDescription}
-				/>
-				<div className="content">
-					<Generals />
-					<div className="d-flex flex-row flex-wrap align-content-center justify-content-center">
-						<Todo title="Todo" todoID="todo-general" addInput deleteBtn />
-						<Timer />
-						<Chrono />
-					</div>
-					<Tracker />
-					<div className="d-flex flex-row flex-wrap align-content-center justify-content-center">
-						<Note />
-						<Questions />
-					</div>
-				</div>
-			</section>
-			<section>
-				<HeaderSection
-					icon={<TfiMapAlt />}
-					title="Maps"
-					description={mapsDescription}
-				/>
-				<div className="content">
-					<Maps />
-				</div>
-			</section>
-			{/* <section>
-				<HeaderSection icon={<FaObjectGroup />} title="Objets" />
-				<div className="content">
-					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-						sed velit sapiente. Corrupti, vel doloremque nemo incidunt impedit
-						molestias enim eos eum voluptatem. Non explicabo minus voluptatum
-						ipsa blanditiis distinctio?
-					</p>
-				</div>
-			</section> */}
+			<Entities_Section />
+			<Maps_Section />
+			<Objects_Section />
 			<ScrollUp />
 		</React.Fragment>
 	);
