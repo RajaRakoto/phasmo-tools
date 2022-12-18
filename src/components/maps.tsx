df058f049d8fdea13df167a6d71d8614925a7afc
raja <raja.rakoto7@gmail.com>
2022-12-18 11:56:44 +0300

    safezone

--- src/components/maps.tsx
+++ src/components/maps.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 /* libs */
 import uuid from 'react-uuid';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 /* data */
 import { maps_general, maps_little } from '../data/maps';
+
+/* common */
+import { IconRender } from '../common/icons';
+import ProgressBar from '../common/progress';
 
 // ================================================
 
@@ -13,21 +18,22 @@ export default function Maps() {
 		<React.Fragment>
 			<div id="map-link" className="maps">
 				<div className="maps-wrapper">
+					{IconRender({ icon: <FaMapMarkerAlt />, size: '42' })}
 					<h2> PETITES MAPS </h2>
 					<ul>
 						{maps_little.map(map => (
 							<li key={uuid()}>
-								<div className="maps-items">
+								<div className="items">
 									<div className="header">
 										<h3>{map.name}</h3>
 										<h4>Prime: {map.prime}$</h4>
-										<img src={map.cover} alt="map cover" width={400}/>
+										<img src={map.cover} alt="map cover" />
 									</div>
 									<div className="body">
-										<button>Cachettes</button>
-										<button>Tips</button>
-										<button>Details</button>
-										<div>
+										<button className="btn-1">Cachettes</button>
+										<button className="btn-2">Tips</button>
+										<button className="btn-3">Details</button>
+										{/* <div>
 											{map.details.map(detail => (
 												<img
 													key={uuid()}
@@ -36,9 +42,16 @@ export default function Maps() {
 													width={200}
 												/>
 											))}
-										</div>
+										</div> */}
 									</div>
-									<div className="footer"></div>
+									<div className="footer">
+										<ProgressBar
+											percent={map.stat.accessibility}
+											label={'Accessibility'}
+										/>
+										<ProgressBar percent={map.stat.level} label={'Level'} />
+										<ProgressBar percent={map.stat.size} label={'Size'} />
+									</div>
 								</div>
 							</li>
 						))}
